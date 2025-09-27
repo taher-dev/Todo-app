10bf13663a351f2148daa00d28c606c81422d87e
taher-dev <monmoyzx@gmail.com>
2025-09-27 12:27:32 +0600

    👌 IMPROVE: Update task delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -13,7 +13,7 @@ export default function TodoItem({ todo, toggleTodo, deleteTodo }) {
         onClick={() => deleteTodo(todo.id)}
         className="ml-3 bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded"
       >
-        ❌
+        ⨉
       </button>
     </li>
   );
